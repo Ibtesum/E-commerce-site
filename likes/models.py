f2d7180ece76b660febe4f806e2e6ea9df9cad46
Ibtesum Reza Aninda <aninda7152@gmail.com>
2022-09-08 03:47:10 +0600

    Upto Generic Relationships

--- likes/models.py
+++ likes/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-
 # Create your models here.
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
 
-class TaggedItem(models.Model):
-    #What tag is applied to what object
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # Type(product, video, article)
-    # ID
+
+# LikedItem
+#  - what user likes what object
+#  - user: ForeignKey to User(django.contrib.auth.models)
+
+class LikedItem(models.Model):
+    # what user likes what object
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
