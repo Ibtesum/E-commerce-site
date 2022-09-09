d19da580fcdbd18dd6aa9dc289e67291549f65bd
Ibtesum Reza Aninda <aninda7152@gmail.com>
2022-09-10 04:20:38 +0600

    upto lesson 40

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from store.models import Product
 # Create your views here.
 
 def say_hello(request):    
-    return render(request, 'hello.html', {'name': 'Aninda'})
+    queryset = Product.objects.filter(title__icontains='coffee')
+
+    return render(
+        request, 'hello.html', {'name': 'Aninda', 'products': list(queryset)})
     
--- storefront/settings.py
+++ storefront/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
